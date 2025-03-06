4b209316f560014791c6d8838723bebb7fe0d773
Eishal <eishal.cecos@gmail.com>
2025-03-06 13:31:43 +0500

    cm
    cm

--- combine.js
+++ combine.js
@@ -1,46 +1,4 @@
-let btn = document.querySelector(".record-btn")
-        let videoPopup = document.getElementById("videoPopup")
-        let recordedVideo = document.getElementById("recordedVideo")
-        let closeBtn = document.querySelector(".close-btn")
-        let mediaRecorder
-        let chunks = []
-        let stream
-        
-        btn.addEventListener("click", async function () {
-            if (btn.textContent === "Start Recording") {
-                stream = await navigator.mediaDevices.getDisplayMedia({ video: true })
-                
-                const mime = MediaRecorder.isTypeSupported("video/webm codecs=vp9") ? "video/webm codecs=vp9" : "video/webm"
-                mediaRecorder = new MediaRecorder(stream, { mimeType: mime })
-                
-                mediaRecorder.ondataavailable = function (e) {
-                    chunks.push(e.data)
-                }
-                
-                mediaRecorder.onstop = function () {
-                    let blob = new Blob(chunks, { type: chunks[0].type })
-                    let url = URL.createObjectURL(blob)
-                    recordedVideo.src = url
-                    videoPopup.style.display = "block"
-                    let a = document.createElement('a')
-                    a.href = url
-                    a.download = 'screen_recording.webm'
-                    a.click()
-                    chunks = []
-                }
-                
-                mediaRecorder.start()
-                btn.textContent = "STOP"
-            } else {
-                mediaRecorder.stop()
-                btn.textContent = "Start Recording"
-            }
-        })
-        
-        closeBtn.addEventListener("click", function () {
-            videoPopup.style.display = "none"
-        })
-        const viewBtn = document.querySelector(".view-modal"), 
+const viewBtn = document.querySelector(".view-modal"), 
     popup = document.querySelector(".popup"),
     close = popup.querySelector(".close"),
     field = popup.querySelector(".field"),
@@ -68,3 +26,51 @@ copy.onclick = () => {
         copy.innerText = "Copy";
     }, 3000);
 };
+
+
+let btn = document.querySelector(".record-btn")
+        let videoPopup = document.getElementById("videoPopup")
+        let recordedVideo = document.getElementById("recordedVideo")
+        let closeBtn = document.querySelector(".close-btn")
+        let mediaRecorder
+        let chunks = []
+        let stream
+        
+        btn.addEventListener("click", async function () {
+            if (btn.textContent === "Start Recording") {
+                stream = await navigator.mediaDevices.getDisplayMedia({ video: true })
+                
+                const mime = MediaRecorder.isTypeSupported("video/webm codecs=vp9") ? "video/webm codecs=vp9" : "video/webm"
+                mediaRecorder = new MediaRecorder(stream, { mimeType: mime })
+                
+                mediaRecorder.ondataavailable = function (e) {
+                    chunks.push(e.data)
+                }
+                
+                mediaRecorder.onstop = function () {
+                    let blob = new Blob(chunks, { type: chunks[0].type })
+                    let url = URL.createObjectURL(blob)
+                    recordedVideo.src = url
+                    videoPopup.style.display = "block"
+                    videoPopup.style.display="block"
+                    let a = document.createElement('a')
+                    a.href = url
+                    a.download = 'screen_recording.webm'
+                    a.click()
+                    chunks = []
+                }
+                
+                mediaRecorder.start()
+                btn.textContent = "STOP"
+            } else {
+                mediaRecorder.stop()
+                btn.textContent = "Start Recording"
+            }
+        })
+        
+        closeBtn.addEventListener("click", function () {
+            videoPopup.style.display = "none"
+        })
+        viewBtn.addEventListener("click",function(){
+            videoPopup.style.display="none"
+        })
